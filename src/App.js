183b0c070d5a9e7b58cc06782c5e09cc404636f0
SELINO VALDES <selinovaldes@SELINOs-MacBook-Pro.local>
2020-02-07 14:55:08 -0800

    Installing and testing redux with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Context } from "react"
 import "./App.css"
+import { useSelector, useDispatch } from "react-redux"
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
   return (
     <div className='App'>
-      <h1>Slektor</h1>
-      <UsersContextProvider>Testing</UsersContextProvider>
+      <header className='App-header'>
+        <h1>Slektor</h1>
+      </header>
+      <div>
+        <h2>Counter: {counter}</h2>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>
+          Decrement
+        </button>
+      </div>
     </div>
   )
 }
