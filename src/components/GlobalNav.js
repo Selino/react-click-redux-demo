f2b908898442b5a96044759dc66a99404f501bb7
Selino Valdes <selinov@gmail.com>
2020-06-06 13:57:19 -0700

    [ch360] Installing router links into the nav.

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -1,33 +1,58 @@
 import React from "react"
-import { Nav, Navbar, Button, NavDropdown } from "react-bootstrap"
+import { Nav, Navbar, NavDropdown } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 import { useDispatch } from "react-redux"
 import { startLogout } from "../actions/auth_actions"
 
 export function GlobalNav() {
   const dispatch = useDispatch()
+  const handleSelect = (eventKey) => {
+    switch (eventKey) {
+      case "logout":
+        dispatch(startLogout())
+        break
+      default:
+        break
+    }
+  }
   return (
-    <Navbar bg='dark' variant='dark'>
-      <Navbar.Brand>Counter Demo</Navbar.Brand>
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='ml-auto'>
-          <Nav.Item>
-            <LinkContainer to='/about'>
-              <Nav.Link>About</Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-          <Nav.Item>
-            <LinkContainer to='/counter'>
-              <Nav.Link>Counter</Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-      <Button onClick={() => dispatch(startLogout())} variant='outline-light'>
-        Logout
-      </Button>
+    <Navbar bg='dark' variant='dark' onSelect={handleSelect}>
+      <Navbar.Brand>Selino Demo</Navbar.Brand>
+      <Nav className='ml-auto'>
+        <NavDropdown title='Menu' alignRight>
+          <LinkContainer to='/about'>
+            <NavDropdown.Item>About</NavDropdown.Item>
+          </LinkContainer>
+          <LinkContainer to='/counter'>
+            <NavDropdown.Item>Counter</NavDropdown.Item>
+          </LinkContainer>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey='logout'>Logout</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
     </Navbar>
   )
 }
 
 export default GlobalNav
+
+// <Navbar bg='dark' variant='dark'>
+// <Navbar.Brand>Selino Demo</Navbar.Brand>
+// <Navbar.Collapse id='basic-navbar-nav'>
+//   <Nav className='ml-auto'>
+//     <Nav.Item>
+//       <LinkContainer to='/about'>
+//         <Nav.Link>About</Nav.Link>
+//       </LinkContainer>
+//     </Nav.Item>
+//     <Nav.Item>
+//       <LinkContainer to='/counter'>
+//         <Nav.Link>Counter</Nav.Link>
+//       </LinkContainer>
+//     </Nav.Item>
+//   </Nav>
+// </Navbar.Collapse>
+// <Button onClick={() => dispatch(startLogout())} variant='outline-light'>
+//   Logout
+// </Button>
+// </Navbar>
