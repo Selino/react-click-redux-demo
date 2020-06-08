6ccdca22ea3a06136d202fc721f4d6a91a3712a5
Selino Valdes <selinov@gmail.com>
2020-06-07 22:47:16 -0700

    Removing wordcount stuff

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -3,7 +3,7 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 import { useDispatch } from "react-redux"
 import { startLogout } from "../actions/auth_actions"
-import { faBars, faHome } from "@fortawesome/free-solid-svg-icons"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "../css/globalnav.css"
 
