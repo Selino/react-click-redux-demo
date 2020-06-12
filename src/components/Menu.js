f721fd00ce6abf377423782e17fb346b887e2a61
Selino Valdes <selinov@gmail.com>
2020-06-11 20:31:58 -0700

    [finish ch411] Making menu an iterating loop.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,6 +7,41 @@ import tnWordCounter from "../images/tn-word-counter.png"
 import tnGifCaller from "../images/tn-gifcaller.gif"
 import styled from "@emotion/styled"
 
+const myData = [
+  {
+    id: "1",
+    link: "/counter",
+    title: "Redux Counter",
+    desc:
+      "It took a while to wrap my head around exactly how Redux works and why it's handy on larger projects. It was helpful to learn by implemting on a simple component. I've also integrated Firebase into the Redux actions.",
+    img: tnReduxCounter,
+  },
+  {
+    id: "2",
+    link: "/executivesummary",
+    title: "Trilliant Executive Summary",
+    desc:
+      "I thought I'd challenge myself by refactoring some of the Trilliant components from Angular into React. It wasn't without issues but it was fast. I literally spent two work days getting the functionality and the look into place.",
+    img: tnExecSummary,
+  },
+  {
+    id: "3",
+    link: "/wordcounter",
+    title: "Word Counter",
+    desc:
+      "I was given a white board test and I didn't have the algorithm for this function committed to memory. It was the only question I was given during a React test. I have it memorized now!",
+    img: tnWordCounter,
+  },
+  ,
+  {
+    id: "4",
+    link: "/gifcaller",
+    title: "Gif Caller",
+    desc: "Wanna hit an API for some GIFs",
+    img: tnGifCaller,
+  },
+]
+
 const Emotion = styled.div`
   .card {
     cursor: pointer;
@@ -31,59 +66,17 @@ export default function About() {
   return (
     <Emotion>
       <div className='d-flex flex-wrap justify-content-center'>
-        <LinkContainer exact to='/counter' active='false'>
-          <Card className='card-override'>
-            <Card.Img variant='top' src={tnReduxCounter} />
-            <Card.Body>
-              <Card.Title>Redux Counter Prototype</Card.Title>
-              <Card.Text>
-                It took a while to wrap my head around exactly how Redux works
-                and why it's handy on larger projects. It was helpful to learn
-                by implemting on a simple component. I've also integrated
-                Firebase into the Redux actions.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </LinkContainer>
-
-        <LinkContainer exact to='/executivesummary' active='false'>
-          <Card className='card-override'>
-            <Card.Img variant='top' src={tnExecSummary} />
-            <Card.Body>
-              <Card.Title>Trilliant Executive Summary Refactor</Card.Title>
-              <Card.Text>
-                I thought I'd challenge myself by refactoring some of the
-                Trilliant components from Angular into React. It wasn't without
-                issues but it was fast. I literally spent two work days getting
-                the functionality and the look into place.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </LinkContainer>
-
-        <LinkContainer exact to='/wordcounter' active='false'>
-          <Card className='card-override'>
-            <Card.Img variant='top' src={tnWordCounter} />
-            <Card.Body>
-              <Card.Title>Word Counter</Card.Title>
-              <Card.Text>
-                I was given a white board test and I didn't have the algorithm
-                for this function committed to memory. It was the only question
-                I was given during a React test. I have it memorized now!
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </LinkContainer>
-
-        <LinkContainer exact to='/gifcaller' active='false'>
-          <Card className='card-override'>
-            <Card.Img variant='top' src={tnGifCaller} />
-            <Card.Body>
-              <Card.Title>Gif Caller</Card.Title>
-              <Card.Text>Wanna hit an API for some GIFs</Card.Text>
-            </Card.Body>
-          </Card>
-        </LinkContainer>
+        {myData.map((a) => (
+          <LinkContainer exact to={a.link} active='false'>
+            <Card className='card-override'>
+              <Card.Img variant='top' src={a.img} />
+              <Card.Body>
+                <Card.Title>{a.title}</Card.Title>
+                <Card.Text>{a.desc}</Card.Text>
+              </Card.Body>
+            </Card>
+          </LinkContainer>
+        ))}
       </div>
     </Emotion>
   )
