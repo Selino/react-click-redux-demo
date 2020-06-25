173fcf25a3eef68882930ea20243683d201c00ad
Selino Valdes <selinov@gmail.com>
2020-06-24 21:38:38 -0700

    [ch416] Installing text area fro description. Broke each display item into a separate component.

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -1,6 +1,14 @@
 import React from "react"
 import { useState, useEffect } from "react"
-import { Form, Button, InputGroup, Container } from "react-bootstrap"
+import {
+  Form,
+  Button,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+  Alert,
+} from "react-bootstrap"
 
 function processNumberArray(array) {
   const processedArray = array.map((item) => {
@@ -12,39 +20,80 @@ function processNumberArray(array) {
   return processedArray
 }
 
+export const AlertItem = (props) => {
+  return (
+    <Alert key={props.key} variant={props.variant}>
+      {props.value}
+    </Alert>
+  )
+}
+
+export const DisplayAlerts = (props) => {
+  let strVariant = ""
+  const result = props.displayArray.map((i, index) => {
+    if (i === "Fizz") strVariant = "warning"
+    else if (i === "Buzz") strVariant = "primary"
+    else if (i === "FizzBuzz") strVariant = "danger"
+    else strVariant = "success"
+    return <AlertItem key={index} variant={strVariant} value={i} />
+  })
+  return <div>{result}</div>
+}
+
 export default function FizzBuzz() {
-  const [number, setNumber] = useState(2)
+  const [number, setNumber] = useState(5)
   const [numberArray, setNumberArray] = useState([])
   const [displayArray, setDisplayArray] = useState([""])
+  const inputRef1 = React.createRef()
+
+  const handleChange = (value) => {
+    setNumber(value)
+  }
 
   useEffect(() => {
     setDisplayArray(processNumberArray(numberArray))
+    inputRef1.current.focus()
   }, [numberArray])
 
   return (
     <Container>
-      <Form
-        onSubmit={(e) => {
-          e.preventDefault()
-          setNumberArray([...numberArray, number])
-        }}
-      >
-        <InputGroup className='mb-3'>
-          <Form.Control
-            type='number'
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
-          />
-          <InputGroup.Append>
-            <Button type='submit'>Submit</Button>
-          </InputGroup.Append>
-        </InputGroup>
-      </Form>
-      <div>
-        {displayArray.map((i, index) => {
-          return <div key={index}>{i}</div>
-        })}
-      </div>
+      <Row>
+        <Col xs={4}>
+          Nullam finibus diam elit, ut sodales augue euismod sed. Mauris sapien
+          lorem, suscipit tempor dictum at, commodo ut libero. Ut felis ipsum,
+          fringilla uis tortor nec, varius pellentesque nibh. Nulla lacinia
+          massa vitae diam lobortis maximus. Curabitur id molestie dolor, mattis
+          fermentum sapien. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Sed efficitur felis vitae
+          suscipit faucibus.
+        </Col>
+
+        <Col xs={3}>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault()
+              setNumberArray([...numberArray, number])
+            }}
+          >
+            <InputGroup className='mb-3'>
+              <Form.Control
+                type='number'
+                value={number}
+                onChange={(e) => {
+                  handleChange(e.target.value)
+                }}
+                ref={inputRef1}
+              />
+              <InputGroup.Append>
+                <Button type='submit'>Check Number</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </Col>
+        <Col xs={2}>
+          <DisplayAlerts displayArray={displayArray} />
+        </Col>
+      </Row>
     </Container>
   )
 }
