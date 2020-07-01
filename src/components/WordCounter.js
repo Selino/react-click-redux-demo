7679b30818c7b1d4c126a284cacedb8601a8e659
Selino Valdes <selinov@gmail.com>
2020-06-30 19:15:53 -0700

    [finish ch471] Looks like emotion causes some effects when using the Emotion tag implenetation.

--- src/components/WordCounter.js
+++ src/components/WordCounter.js
@@ -1,64 +1,58 @@
 import React, { useState, useEffect } from "react"
 import { Row, Col } from "react-bootstrap"
 import { Fade } from "react-reveal"
-import styled from "@emotion/styled"
 
 export default function WordCounter() {
-  const [state, setState] = useState(0)
+  const [count, setCount] = useState(0)
   const [words, setWords] = useState("")
 
   useEffect(() => {
     let regex = new RegExp(/\w/)
     const aRaw = words.split(" ")
     const clean = aRaw.filter((e) => e.match(regex))
-    setState(clean.length)
+    setCount(clean.length)
   }, [words])
 
-  const Emotion = styled.div`
-    textarea {
-      width: 100%;
-      min-height: 200px;
-      resize: none;
-    }
-  `
+  const handleChange = (e) => {
+    setWords(e.target.value)
+  }
 
   return (
-    <Emotion>
-      <Fade left>
-        <Row>
-          <Col xs={12} sm={6}>
-            <textarea
-              id='name'
-              type='textarea'
-              placeholder='Type text into this area.'
-              value={words}
-              onChange={(e) => setWords(e.target.value)}
-            />
-            <div>{state}</div>
-          </Col>
-          <Col>
-            <p>
-              It took a while to figure out the method & regex combo to filter
-              out the non-letters. I don't enjoy Regex.
-            </p>
-            <code>
-              useEffect(() => &#123;
-              <br />
-              &nbsp; let regex = new RegExp(/\w/)
-              <br />
-              &nbsp; const aRaw = str.split(" ")
-              <br />
-              &nbsp; const clean = aRaw.filter((e) =>
-              <br />
-              &nbsp; e.match(regex))
-              <br />
-              &nbsp; setState(clean.length)
-              <br />
-              &#125;, [words])
-            </code>
-          </Col>
-        </Row>
-      </Fade>
-    </Emotion>
+    <Fade left>
+      <Row>
+        <Col xs={12} sm={6}>
+          <textarea
+            id='name'
+            value={words}
+            type='textarea'
+            placeholder='Type text into this area.'
+            onChange={(e) => handleChange(e)}
+            style={{ width: "100%", minHeight: "200px", resize: "none" }}
+          />
+          <div>{count}</div>
+        </Col>
+        <Col>
+          <p>
+            It took a while to figure out the method & regex combo to filter out
+            the non-letters. I don't enjoy Regex.
+          </p>
+          <code>
+            useEffect(() => &#123;
+            <br />
+            &nbsp; let regex = new RegExp(/\w/)
+            <br />
+            &nbsp; const aRaw = str.split(" ")
+            <br />
+            &nbsp; const clean = aRaw.filter((e) =>
+            <br />
+            &nbsp; e.match(regex))
+            <br />
+            &nbsp; setState(clean.length)
+            <br />
+            &#125;, [words])
+          </code>
+        </Col>
+      </Row>
+    </Fade>
   )
 }
