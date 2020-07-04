bddf7beb6d1e62a03e6c64ffc5c8c261ed98024f
Selino Valdes <selinov@gmail.com>
2020-07-03 20:08:34 -0700

    Adding config files to support enzyme.

--- src/components/fizzbuzz/FizzBuzz.test.js
+++ src/components/fizzbuzz/FizzBuzz.test.js
@@ -1,7 +1,15 @@
 import React from "react"
-import { getString } from "./FizzBuzz"
+import { shallow } from "enzyme"
+import { getString, AlertItem } from "./FizzBuzz"
 
 test("return Fizz if % 3, Buzz if % 5, FIZZBUZZ! if both.", () => {
   const action = getString(15)
   expect(action).toBe("FIZZBUZZ!")
+})
+
+test("should render a single Alert item", () => {
+  const wrapper = shallow(
+    <AlertItem key='1' variant='warning' displayStatus='Fizz' value={3} />
+  )
+  expect(wrapper).toMatchSnapshot()
 })
