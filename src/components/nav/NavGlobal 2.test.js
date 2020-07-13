9e2dfd8d1ea6ebc9e36fa900c46aae71f2d0e653
Selino Valdes <selinov@gmail.com>
2020-07-13 10:36:37 -0700

    [ch485] Adding snapshot test.

---
+++ src/components/nav/NavGlobal 2.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { render, fireEvent, screen } from "../../test-utils"
+import "@testing-library/jest-dom/extend-expect"
+import NavGlobal from "./NavGlobal"
+
+describe("NavGlobal", () => {
+  test("should match snapshot", () => {})
+})
