af2ef47f630f388c82a9035a08395361758d30ea
Selino Valdes <selinov@gmail.com>
2020-08-03 11:10:25 -0700

    [ch511] Updating tests.

--- src/components/nav/NavSub.test.js
+++ src/components/nav/NavSub.test.js
@@ -2,23 +2,23 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import NavSub from "./NavSub"
-import { getSubMenuText } from "../../fixtures/SubMenuText"
+import { getMenuData } from "../../fixtures/MenuText"
 
 console.warn = jest.fn()
 
 describe("NavSub", () => {
-  test("should match snapshot for each location from the getSubMenuText array", () => {
-    getSubMenuText().forEach((item) => {
+  test("should match snapshot for each location from the getMenuData array", () => {
+    getMenuData().forEach((item) => {
       const { asFragment } = render(
-        <NavSub location={{ pathname: item.url }} />
+        <NavSub location={{ pathname: item.link }} />
       )
       expect(asFragment()).toMatchSnapshot()
     })
   })
 
-  test("should match title and desc strings from getSubMenuText array", () => {
-    getSubMenuText().forEach((item) => {
-      render(<NavSub location={{ pathname: item.url }} />)
+  test("should match title and desc strings from getMenuData array", () => {
+    getMenuData().forEach((item) => {
+      render(<NavSub location={{ pathname: item.link }} />)
       expect(screen.getAllByText(item.title))
     })
   })
