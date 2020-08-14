ef217c047a059e5cdb37d1865dde00f12bc239cc
Selino Valdes <selinov@gmail.com>
2020-08-14 15:05:24 -0700

    Fixing video image resizing on tablets.

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,7 +55,7 @@ function CardItem({ title, desc, img, video, link }) {
             <Card.Img variant='top' src={img} />
           ) : (
             <video
-              playsinline='playsinline'
+              playsInline='playsinline'
               autoPlay={true}
               loop={true}
               src={video}
