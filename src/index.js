62a9a12229549916fac20224ce5e4ab75ae40225
Selino Valdes <selinov@gmail.com>
2020-08-27 12:31:29 -0700

    [ch529] Adding summary text.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ firebase.auth().onAuthStateChanged((user) => {
     renderApp()
     store.dispatch(login(user.uid))
     if (history.location.pathname === "/") {
-      history.push("/wordcounter")
+      history.push("/home")
     }
   } else {
     renderApp()
