1836f07a31e6c8d77ebf4a05e5ba5aae27c8b879
Selino Valdes <selinov@gmail.com>
2021-02-22 11:14:41 -0800

    [ch711] Trying to fix AppRouter fail on Nav call.

--- src/components/counter/Counter.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { createStore } from "redux"
-import { render, fireEvent, screen } from "../../test-utils"
-import "@testing-library/jest-dom/extend-expect"
-import Counter from "./counter"
-
-describe("Counter", () => {
-  test("can render with redux with defaults", () => {})
-})
--- src/components/nav/NavGlobal.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { render, fireEvent, screen } from "../../test-utils"
-import "@testing-library/jest-dom/extend-expect"
-import NavGlobal from "./NavGlobal"
-
-describe("NavGlobal", () => {
-  test("should match snapshot", () => {})
-})
