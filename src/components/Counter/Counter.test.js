85b5abcf89ebc0d69a147e5406dd204170f04d52
Selino Valdes <selinov@gmail.com>
2021-02-22 11:16:26 -0800

    [ch711] Trying to fix AppRouter fail on Nav call.

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { createStore } from "redux"
+import { render, fireEvent, screen } from "../../test-utils"
+import "@testing-library/jest-dom/extend-expect"
+import Counter from "./counter"
+
+describe("Counter", () => {
+  test("can render with redux with defaults", () => {})
+})
